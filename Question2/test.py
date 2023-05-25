ae698f2056f4023713149f05feb9be5dc2111e78
Jaimerpb <113980728+Jaimerpb@users.noreply.github.com>
2023-05-25 08:57:36 +0000

    pokemon

--- Question2/test.py
+++ Question2/test.py
@@ -1,1 +1,7 @@
-import unittest 
+import unittest
+from pokemon import pokemon  
+class Pokemon:
+    def __init__(self, nombre, tipo):
+        self.nombre = nombre
+        self.tipo = tipo
+        print(f"Se ha creado el Pokémon {self.nombre} de tipo {self.tipo} con éxito.")
