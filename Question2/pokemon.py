426d17b889c884edffdba4711cf6caf6038aa4be
Jaimerpb <113980728+Jaimerpb@users.noreply.github.com>
2023-05-25 08:26:27 +0000

    question2

--- Question2/pokemon.py
+++ Question2/pokemon.py
@@ -1,1 +1,60 @@
+class Pokemon:
+    def __init__(self, nombre, tipo):
+        self.nombre = nombre
+        self.tipo = tipo
+        print(f"Se ha creado el Pokémon {self.nombre} de tipo {self.tipo} con éxito.")
+
+    def clasificacion(self):
+        if self.tipo == "Agua":
+            print("Clasificación:")
+            print("- PS: Puntos de Salud")
+            print("- Ataque")
+            print("- Defensa")
+            print("- Ataque Especial")
+            print("- Defensa Especial")
+            print("- Velocidad")
+        elif self.tipo == "Fuego":
+            print("Clasificación:")
+            print("- PS: Puntos de Salud")
+            print("- Ataque")
+            print("- Defensa")
+            print("- Ataque Especial")
+            print("- Defensa Especial")
+            print("- Velocidad")
+        elif self.tipo == "Tierra":
+            print("Clasificación:")
+            print("- PS: Puntos de Salud")
+            print("- Ataque")
+            print("- Defensa")
+            print("- Ataque Especial")
+            print("- Defensa Especial")
+            print("- Velocidad")
+        elif self.tipo == "Eléctrico":
+            print("Clasificación:")
+            print("- PS: Puntos de Salud")
+            print("- Ataque")
+            print("- Defensa")
+            print("- Ataque Especial")
+            print("- Defensa Especial")
+            print("- Velocidad")
+        elif self.tipo == "Normal":
+            print("Clasificación:")
+            print("- PS: Puntos de Salud")
+            print("- Ataque")
+            print("- Defensa")
+            print("- Ataque Especial")
+            print("- Defensa Especial")
+            print("- Velocidad")
+        elif self.tipo == "Fantasma":
+            print("Clasificación:")
+            print("- PS: Puntos de Salud")
+            print("- Ataque")
+            print("- Defensa")
+            print("- Ataque Especial")
+            print("- Defensa Especial")
+            print("- Velocidad")
+        else:
+            print("Tipo de Pokémon desconocido.")
+
+
 
