0f0b360127a57234f3b4980b7a5af5d92ddc495b
Jaimerpb <113980728+Jaimerpb@users.noreply.github.com>
2023-05-25 08:52:27 +0000

    Tipo electrico y tipo normal

--- Question2/pokemon.py
+++ Question2/pokemon.py
@@ -23,4 +23,28 @@ class Pokemon:
             print("- Defensa Especial")
             print("- Velocidad")
 
+        elif self.tipo == "Tierra":
+            print("Clasificación:")
+            print("- PS: Puntos de Salud")
+            print("- Ataque")
+            print("- Defensa")
+            print("- Ataque Especial")
+            print("- Defensa Especial")
+            print("- Velocidad")
 
+        elif self.tipo == "Eléctrico":
+            print("Clasificación:")
+            print("- PS: Puntos de Salud")
+            print("- Ataque")
+            print("- Defensa")
+            print("- Ataque Especial")
+            print("- Defensa Especial")
+            print("- Velocidad")
+        elif self.tipo == "Normal":
+            print("Clasificación:")
+            print("- PS: Puntos de Salud")
+            print("- Ataque")
+            print("- Defensa")
+            print("- Ataque Especial")
+            print("- Defensa Especial")
+            print("- Velocidad")
